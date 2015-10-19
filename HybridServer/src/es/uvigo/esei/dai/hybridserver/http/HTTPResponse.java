a5c98cbff981c0f3615307ff7147a2852e96d25a
Salvador Sánchez Méndez <salvador.sanchez.mendez@gmail.com>
2015-10-20 00:47:00 +0200

    HTTPResponse
    Funcionan la mayoría de test.
    Falta hacer los casos en los que se soliciten todos los parámetros.

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java
@@ -3,78 +3,115 @@ package es.uvigo.esei.dai.hybridserver.http;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HTTPResponse {
-	public HTTPResponse() {
-	}
 
-	public HTTPResponseStatus getStatus() {
-		// TODO Auto-generated method stub
-		return null;
-		
-	}
+    private HTTPResponseStatus status;
+    private String version;
+    private String contenido;
+    private Map<String, String> parametros = new LinkedHashMap<String, String>();
 
-	public void setStatus(HTTPResponseStatus status) {
-	}
+    public HTTPResponse() {
+    }
 
-	public String getVersion() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public HTTPResponseStatus getStatus() {
+        // TODO Auto-generated method stub
+        return status;
+    }
 
-	public void setVersion(String version) {
-	}
+    public void setStatus(HTTPResponseStatus status) {
+        this.status = status;
+    }
 
-	public String getContent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String getVersion() {
+        // TODO Auto-generated method stub
+        return version;
+    }
 
-	public void setContent(String content) {
-	}
+    public void setVersion(String version) {
+        this.version = version;
+    }
 
-	public Map<String, String> getParameters() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String getContent() {
+        // TODO Auto-generated method stub
+        return contenido;
+    }
 
-	public String putParameter(String name, String value) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void setContent(String content) {
+        this.contenido = content;
+    }
 
-	public boolean containsParameter(String name) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public Map<String, String> getParameters() {
+        // TODO Auto-generated method stub
+        return parametros;
+    }
 
-	public String removeParameter(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String putParameter(String name, String value) {
+        parametros.put(name, value);
+        return parametros.get(name);
+    }
 
-	public void clearParameters() {
-	}
+    public boolean containsParameter(String name) {
+        // TODO Auto-generated method stub
+        return this.parametros.containsKey(name);
+    }
 
-	public List<String> listParameters() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String removeParameter(String name) {
+        // TODO Auto-generated method stub
+        return this.parametros.remove(name);
+    }
 
-	public void print(Writer writer) throws IOException {
-	}
+    public void clearParameters() {
+        this.parametros.clear();
+    }
 
-	@Override
-	public String toString() {
-		final StringWriter writer = new StringWriter();
+    public List<String> listParameters() {
+        // TODO Auto-generated method stub
+        List<String> tr = new ArrayList<String>();
 
-		try {
-			this.print(writer);
-		} catch (IOException e) {
-		}
+        for (Map.Entry<String, String> elemento : this.parametros.entrySet()) {
+            tr.add(elemento.getKey());
+        }
 
-		return writer.toString();
-	}
+        return tr;
+    }
+
+    public void print(Writer writer) throws IOException {
+
+        System.out.println(parametros.size());
+        writer.write(this.version + " " + this.status.getCode() + " " + this.status.getStatus() + "\r\n");
+        for (Map.Entry<String, String> elemento : this.parametros.entrySet()) {
+            writer.write(elemento.getKey() + ": " + elemento.getValue() + "\r\n\r\n");
+            System.out.println(elemento.getKey());
+        }
+        
+        
+        if (contenido != null){
+            writer.write("Content-Length: " + contenido.length() + "\r\n");
+            writer.write("\r\n");
+            writer.write(contenido);
+        }else{
+            writer.write("\r\n");
+        }
+        writer.close();
+    }
+
+    @Override
+    public String toString() {
+        final StringWriter writer = new StringWriter();
+
+        try {
+            this.print(writer);
+
+        } catch (IOException e) {
+        }
+
+        return writer.toString();
+    }
 }
+
+
